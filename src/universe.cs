f87f36dc77257fcbb4e12e40bb51c8c205ccb1c7
Ted Stein <karamarisan@gmail.com>
2011-12-11 14:18:27 -0600

    Add Universe.

---
+++ src/universe.cs
@@ -0,0 +1,28 @@
+/*
+ * A representation of a universe.
+ */
+
+using System;
+using System.Collections.Generic;
+
+
+public class Universe
+{
+    // Actors.
+    CoordinateEngine.RelativisticObject bro = new
+        CoordinateEngine.RelativisticObject(0, 0, 0);
+    List<CoordinateEngine.RelativisticObject> dudes = new
+        List<CoordinateEngine.RelativisticObject>();
+
+    // Accounting.
+    private DateTime start_time;
+    private DateTime current_frame;
+    private TimeSpan elapsed_time;
+    
+
+    public List<CoordinateEngine.RelativisticObject> getNPCs()
+    {
+        return dudes;
+    }
+}
+
